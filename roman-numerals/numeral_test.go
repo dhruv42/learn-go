5c50d97305355f89a54bfe323d4aa310a829c461
dhruv42 <dhruvpopat42@gmail.com>
2021-12-09 11:49:22 +0530

    Refactor test cases and ConvertToRoman function.

--- roman-numerals/numeral_test.go
+++ roman-numerals/numeral_test.go
@@ -3,10 +3,26 @@ package romannumerals
 import "testing"
 
 func TestRomanNumerals(t *testing.T) {
-	got := ConvertToRoman(1)
-	want := "I"
 
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
+	cases := []struct {
+		Description string
+		Arabic      int
+		Want        string
+	}{
+		{"1 gets converted to I", 1, "I"},
+		{"2 gets converted to I", 2, "II"},
+		{"3 gets converted to III", 3, "III"},
+		{"4 gets converted to IV (can't repeat more than 3 times)", 4, "IV"},
+		{"5 gets converted to V", 5, "V"},
+		{"6 gets converted to VI", 6, "VI"},
+	}
+
+	for _, test := range cases {
+		t.Run(test.Description, func(t *testing.T) {
+			got := ConvertToRoman(test.Arabic)
+			if got != test.Want {
+				t.Errorf("got %q, want %q", got, test.Want)
+			}
+		})
 	}
 }
