6fbaaeaad1cbaed98b32ffc055e88d13a57901c1
dhruv42 <dhruvpopat42@gmail.com>
2021-12-10 11:58:45 +0530

    Add TestPropertyBasedConversion.

--- roman-numerals/numeral_test.go
+++ roman-numerals/numeral_test.go
@@ -3,6 +3,7 @@ package romannumerals
 import (
 	"fmt"
 	"testing"
+	"testing/quick"
 )
 
 var cases = []struct {
@@ -59,5 +60,21 @@ func TestConvertingToArabic(t *testing.T) {
 				t.Errorf("got %d, want %d", got, test.Arabic)
 			}
 		})
+	}
+}
+
+func TestPropertiesOfConversion(t *testing.T) {
+	assertion := func(arabic int) bool {
+		if arabic > 3999 {
+			return true
+		}
+		t.Log("testing", arabic)
+		roman := ConvertToRoman(arabic)
+		fromRoman := ConvertToArabic(roman)
+		return fromRoman == arabic
+	}
+
+	if err := quick.Check(assertion, nil); err != nil {
+		t.Error("Failed checks", err)
 	}
 }
